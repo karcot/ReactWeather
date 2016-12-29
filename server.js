13d09a14cf2d4a3b99f58bcdeea7f7af4fdab62e
karcot <Arcot@KarthikeyansMBP.fios-router.home>
2016-12-28 21:39:25 -0500

    prep repo for heroku

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ var express = require ('express');
 
 var app = express();
 const PORT = process.env.PORT || 3000;
-app.use(fucntion (req, res, next) {
+
+app.use (fucntion (req, res, next) {
+
   if (req.headers ['x-forwarded-proto']=== 'http' ) {
     next();
 
-  } else{
-    res.redirect('http://'+req.hostname + req.url);
+  } else {
+    res.redirect('http://'+ req.hostname + req.url);
   }
 
 });
-
 app.use(express.static('public'));
 app.listen (PORT, function () {
   console.log ('express server is up and running on port' +  PORT);
