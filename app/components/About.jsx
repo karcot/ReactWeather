3796bb3d9d6fba1d6288bbd812f78ad36fceb82e
karcot <Arcot@Karthikeyans-MacBook-Pro.local>
2017-01-12 11:36:50 -0500

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,23 @@ var React = require('react');
 
 var About =  (props) => {
   return (
-  <h3>About Component</h3>
+    <div>
+  <h1 className ="text-center">About Component</h1>
+  <p> This is a weather app</p>
+  <p>
+    Here are some of the tools
+    <ul>
+      <li>
+        <a href="https://facebook.github.io/react"> JS framwework</a>
+      </li>
+      <li>
+        <a href = "http://openweathermap.org"> used this</a>
+      </li>
+    </ul>
+  </p>
+</div>
+
+
   )
 };
 module.exports = About;
